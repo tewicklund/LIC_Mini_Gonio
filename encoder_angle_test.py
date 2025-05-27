f634ef93858f804e57b18533797e4a79e988f8b8
tewicklund <tewicklund@ucdavis.edu>
2025-05-27 13:22:26 -0700

    enter full command

--- encoder_angle_test.py
+++ encoder_angle_test.py
@@ -1,7 +1,7 @@
 import serial
 import time
 
-command=input('Enter Command without @0 or /r/n: ')
+command=input('Enter Full Command: ')
 # Open the serial port
 ser = serial.Serial(
     port='/dev/ttyUSB0',  # or COMx on Windows
@@ -13,7 +13,6 @@ ser = serial.Serial(
 )
 
 # Send a known-good command (e.g., read inputs)
-command = '@0'+command+'\r\n'
 ser.write(command.encode())
 time.sleep(0.1)
 
