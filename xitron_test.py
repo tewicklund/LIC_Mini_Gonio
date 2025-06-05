f535c99c8509cc4b1941a4db393ad383c5fd0bcb
tewicklund <tewicklund@ucdavis.edu>
2025-06-05 13:32:26 -0700

    improve xitron test

--- xitron_test.py
+++ xitron_test.py
@@ -2,4 +2,6 @@ from xitron_functions import *
 
 xitron_serial=xitron_establish_serial_connection('/dev/ttyUSB2')
 
-xitron_send_command("IDN?\n",xitron_serial)
+response = xitron_send_command("IDN?\n",xitron_serial)
+
+print(response)
