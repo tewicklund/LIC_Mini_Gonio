8e429778987793c963368a8deab77a5df291336d
tewicklund <tewicklund@ucdavis.edu>
2025-06-06 13:30:25 -0700

    removed arduino from calibration

--- calibrate.py
+++ calibrate.py
@@ -8,7 +8,7 @@ dimming_arduino_port_name='/dev/ttyACM0'
 #establish serial comms
 PCL_serial=PCL_establish_serial_connection(PCL_port_name)
 t10a_serial=T10A_establish_serial_connection(T10A_port_name)
-arduino_serial=dimming_arduino_establish_serial_connection(dimming_arduino_port_name)
+#arduino_serial=dimming_arduino_establish_serial_connection(dimming_arduino_port_name)
 t10a_init(t10a_serial)
 
 #turn on center light
@@ -27,8 +27,8 @@ PCL_go_to_angle(PCL_serial,angle_steps)
 #     time.sleep(1)
 
 while True:
-    dimming_voltage_value=get_voltage_from_arduino(arduino_serial)
-    print(f"Arduino Voltage: {dimming_voltage_value}")
+    #dimming_voltage_value=get_voltage_from_arduino(arduino_serial)
+    #print(f"Arduino Voltage: {dimming_voltage_value}")
     lx_value=t10a_get_lx_measurement(t10a_serial)
     lx_value_rounded=round(lx_value,2)
     print(f"Lux Value: {lx_value_rounded}")
