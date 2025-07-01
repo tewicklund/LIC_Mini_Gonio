92d86b3196100381e0aae52a17968d8533a9d0a2
tewicklund <tewicklund@ucdavis.edu>
2025-07-01 14:00:29 -0700

    changed main so the user does not have to input USB connection ports

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parameters_from_user_functions import *
 from run_test_function import *
+from USB_functions import *
 
 
 warm_up_time_seconds=180
@@ -18,11 +19,7 @@ else:
     log_file_name='mini_gonio_demo_log.csv'
 
 #COM port names: plug in PCL first, then T10A, then arduino
-PCL_port_name='/dev/ttyUSB1'
-T10A_port_name='/dev/ttyUSB0'
-#dimming_arduino_port_name='/dev/ttyACM0'
-xitron_serial_port_name='/dev/ttyUSB2'
-
+[PCL_port_name,T10A_port_name,xitron_serial_port_name]=get_port_names()
 
 
 #run test
