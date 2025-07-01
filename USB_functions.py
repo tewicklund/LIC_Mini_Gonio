6071a49a65de834b800b067b02ba9294234e346e
tewicklund <tewicklund@ucdavis.edu>
2025-07-01 12:50:06 -0700

    made script for auto identifying USB devices

---
+++ USB_functions.py
@@ -0,0 +1,28 @@
+import serial.tools.list_ports
+
+# Get a list of available ports
+COM_ports = serial.tools.list_ports.comports()
+
+for COM_port in COM_ports:
+    # Check if port is PCL
+    try:
+        #Open PCL serial port
+        PCL_serial = serial.Serial(
+            port=COM_port.device,  # or COMx on Windows
+            baudrate=38400,
+            bytesize=serial.EIGHTBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1  # seconds
+        )
+        identify_command="*IDN?\r"
+        PCL_serial.write(identify_command.encode())
+
+        # Read the response
+        response = PCL_serial.read(64)  # Adjust byte count as needed
+        print("Response:", response.decode(errors='ignore'))
+
+        print(f"PCL found on {COM_port.device}")
+
+    except:
+        print(f"PCL not found on {COM_port.device}")
