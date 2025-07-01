905562fa228a54815ef9b4ef4dc458e54a768e26
tewicklund <tewicklund@ucdavis.edu>
2025-07-01 16:05:13 -0700

    refreshed calibration code

--- calibrate.py
+++ calibrate.py
@@ -1,8 +1,8 @@
 from run_test_function import *
+from USB_functions import *
 
 #serial port names
-PCL_port_name='/dev/ttyUSB1'
-T10A_port_name='/dev/ttyUSB0'
+[PCL_port_name,T10A_port_name,xitron_serial_port_name]=get_port_names()
 
 #establish serial comms
 PCL_serial=PCL_establish_serial_connection(PCL_port_name)
@@ -18,15 +18,8 @@ angle_steps=int(angle/0.009)
 PCL_home_motor(PCL_serial)
 PCL_go_to_angle(PCL_serial,angle_steps)
 
-#wait 3 minutes for lamp to warm up
-# print("Lamp warming up")
-# for second in range(180):
-#     print(180-second)
-#     time.sleep(1)
 
 while True:
-    #dimming_voltage_value=get_voltage_from_arduino(arduino_serial)
-    #print(f"Arduino Voltage: {dimming_voltage_value}")
     lx_value=t10a_get_lx_measurement(t10a_serial)
     lx_value_rounded=round(lx_value,2)
     print(f"Lux Value: {lx_value_rounded}")
