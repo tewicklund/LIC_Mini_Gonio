8675599f4b904ccebdf5dc3283687836d4953539
tewicklund <tewicklund@ucdavis.edu>
2025-07-01 13:36:29 -0700

    remove decode from xitron

--- xitron_functions.py
+++ xitron_functions.py
@@ -14,7 +14,7 @@ def xitron_establish_serial_connection(serial_port):
 def xitron_send_command(command_string,xitron_serial):
     xitron_serial.write(command_string.encode("ascii"))
     xitron_serial.write("READ?\n".encode("ascii"))
-    received_string=xitron_serial.readline().decode()
+    received_string=xitron_serial.readline()#.decode()
     return received_string
 
 # Configure the serial port for the XT2640
